dc4c295d684536d3709547c10fa399fa07fe0b73
rk4an <erkan2005@gmail.com>
2013-05-19 17:54:46 +0200

    express examples

--- socket_io/app2.js
+++ socket_io/app2.js
@@ -2,31 +2,36 @@ var http    =   require('http');
 var fs      =   require('fs');
 var net     =   require('net');
 var io      =   require('socket.io');
+var express =   require('express');
 
 var HOST = '127.0.0.1';
 var PORT = 6969;
 
-var messages = [];
-
 var client = new net.Socket();
 
-    client.connect(PORT, HOST, function() {
-	    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    });
+client.connect(PORT, HOST, function() {
+	console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+});
 
-var app = http.createServer(function (req, res) {
+/*var app = http.createServer(function (req, res) {
     fs.readFile('./app2.html', 'utf-8', function(error, content) {
         res.writeHead(200, {'Content-Type' : 'text/html'});
         res.end(content);
     });
 });
+*/
 
+var exp = express();
+var app = http.createServer(exp);
+exp.use(express.static(__dirname + '/public'));
+exp.get('/', function (req, res) {
+  res.sendfile(__dirname + '/app2.html');
+});
 
 io = io.listen(app); 
 
 //client connect
 io.sockets.on('connection', function (socket) {
-
 
     client.on('data', function(data) {
 		socket.emit('getNew', {'message' : data.toString() });
