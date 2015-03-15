77cb5b4f5371285d21a8bf312a18325a8967f5a9
rk4an <erkan2005@gmail.com>
2015-03-15 13:35:41 +0100

    update rss feed Francko kernel for Nexus5/Android 5.1

--- php/rss/franco_rss.php
+++ php/rss/franco_rss.php
@@ -11,20 +11,20 @@ echo '<description>Nexus 5 franco.Kernel release</description>';
 $date =  new DateTime();
 echo '<pubDate>'. $date->format(DateTime::RSS) .'</pubDate>';
 
-$changelog = file_get_contents('http://kernels.franco-lnx.net/Nexus5/5.0/appfiles/changelog.xml');
+$changelog = file_get_contents('http://kernels.franco-lnx.net/Nexus5/5.1/appfiles/changelog.xml');
 
 $versions = simplexml_load_string($changelog);
 
 for($i=0;$i<10;$i++) {
     echo '<item>';
         echo '<title>Nexus5 Franco Kernel '.$versions->changelogversion[$i]['versionName']. ' - ' . $versions->changelogversion[$i]['changeDate'] .'</title>';
-        echo '<link>http://kernels.franco-lnx.net/Nexus5/4.4/appfiles/changelog.xml</link>';
+        echo '<link>http://kernels.franco-lnx.net/Nexus5/5.1/appfiles/changelog.xml</link>';
         echo '<description>';
           for($j=0; $j<count($versions->changelogversion[$i]->changelogtext); $j++) {
               echo $versions->changelogversion[$i]->changelogtext[$j] . "\n";
           }
         echo '</description>';
-        echo '<guid>http://kernels.franco-lnx.net/Nexus5/5.0/appfiles/changelog.xml?'.$versions->changelogversion[$i]['versionName'].'</guid>';
+        echo '<guid>http://kernels.franco-lnx.net/Nexus5/5.1/appfiles/changelog.xml?'.$versions->changelogversion[$i]['versionName'].'</guid>';
     echo '</item>';
 
 }
